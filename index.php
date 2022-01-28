6af79129e4312ed630cb0889494112c9446adf7a
Feiju12138 <903749471@qq.com>
2022-01-28 23:41:46 +0800

    修改首页名为bilibili-video

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 <meta charset="UTF-8">
-<title>DiandianTV -as</title>
+<title>bilibili-video</title>
 <link rel="stylesheet" href="css/yzmplayer.css">
 <style> 
 .yzmplayer-full-icon span svg,.yzmplayer-fulloff-icon span svg{display: none;}
